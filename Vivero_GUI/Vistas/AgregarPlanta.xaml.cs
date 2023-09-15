72f4992d7363b365998b8a1920ac14d806c42c16
Cristian <kristian13orellanagalaz@gmail.com>
2023-09-15 12:58:30 -0300

    first comit, create WPF views(add,update/list)

---
+++ Vivero_GUI/Vistas/AgregarPlanta.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vivero_GUI.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para AgregarPlanta.xaml
+    /// </summary>
+    public partial class AgregarPlanta : Window
+    {
+        public AgregarPlanta()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
